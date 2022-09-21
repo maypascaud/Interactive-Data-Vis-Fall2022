c877337478b849fc117934e66ae68acbe273540d
May Pascaud <maypascaud@mays-mbp.mynetworksettings.com>
2022-09-21 00:12:29 -0400

    hiiii

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,9 @@
 console.log('hello world');
+
+let count = 0;
+
+const updateCounter = () => {
+    count = count +1;
+    //console.log(count);
+    document.getElementById("count").innerHTML = count
+}; 
